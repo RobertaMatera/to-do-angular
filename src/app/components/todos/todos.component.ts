12fe947227d2512910f4ebe2fd342d42a740e078
RobertaMatera <materaroberta11@gmail.com>
2022-02-16 16:49:47 +0100

    add to do functionality implemented

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -36,4 +36,11 @@ export class TodosComponent implements OnInit {
   deleteTodo(id:number) {
     this.todos = this.todos.filter((todo, i) => i !== id)
   }
+
+  addTodo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    })
+  }
 }
